4cd3ffd0625d4885005920dbe1fcf74c9d58451f
malay1803 <18acharyamalay@gmail.com>
2021-05-29 15:00:00 +0530

    Styled the App

--- src/components/StatusMessage.js
+++ src/components/StatusMessage.js
@@ -3,8 +3,22 @@ import React from 'react'
 const StatusMessage = ({winner, current}) => {
     const noMovesLeft = current.board.every((el)=>el!==null)
 
-    return <h2>{winner && `Winner is ${winner}`}{!winner && !noMovesLeft && `Next Player is ${current.isXNext? 'X':'O'}`}
-    { !winner && noMovesLeft && "It's a Draw"}</h2>
+    return( 
+        <div className="status-message">
+            {winner && <>
+                Winner is <span className={winner == 'X'? 'text-green' : 'text-orange'}>{winner}</span>
+            </>
+            }
+            {!winner && 
+            !noMovesLeft && 
+            <>
+            Next Player is <span className={current.isXNext? 'text-green' : 'text-orange'}>{current.isXNext? 'X':'O'}</span>
+            </>}
+            { !winner && noMovesLeft && <>
+                <span className="text-green">X</span> and <span className="text-orange">O</span> tied
+            </>}
+        </div>
+    )
 }
 
 export default StatusMessage;
